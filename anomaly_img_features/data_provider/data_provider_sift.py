a85be966d54cc8c8b47e9e6b9f94f229265b3f89
harpreetsingh <harpreets@nevada.unr.edu>
2018-04-05 14:37:53 -0700

    feature extraction and clustering code

--- anomaly_img_features/data_provider/data_provider_sift.py
+++ anomaly_img_features/data_provider/data_provider_sift.py
@@ -1,37 +1,43 @@
 import os
-import cv2 as cv
+import cv2
+from cv2 import xfeatures2d as nonfree
+import math
 
 
 class DataProviderSURF(object):
     def __init__(self, training_images_dir, **kwargs):
         """
-        This data provider utilizes the visual bag of words algorithm to map images to feature vectors for the one-class
-        SVM classifier.
+        This data provider utilizes the visual bag of words algorithm to map image_file
+        to feature vectors for the one-class SVM classifier.
         Process:
             - Partition each image into a grid and generate SURF descriptor from each patch
-            - Compute K clusters from all of the features from all of the images (visual bag of words)
+            - Compute K clusters from all of the features from all of the image_file (visual bag of words)
             - Construct normalized histogram for each image
             - Feature vector is then the values of the normalized histogram (vector quantization)
 
         :param training_images_dir: (string)
         :param kwargs:
             - num_clusters: (Integer) Size of the visual bag of words
-            - resize_image: (tuple(x, y)) If input images need to be resized
-            - grid_size: (tuple(x, y)) number of patches (i.e. number of features per image)
+            - resize_image: (tuple(x, y)) resize input image
+            - patch_size: (Integer) size of patch to compute a descriptor
         """
 
         # note~ not much arg validation here...
-
-        self._num_clusters = kwargs.pop("num_clusters", 200)
         self._resize_image = kwargs.pop("resize_image", ())
-        self._grid_size = kwargs.pop("grid_size", ())
+        self._patch_size = kwargs.pop("patch_size", 16)
 
-        features_tensor = []
-        # extract features for all the images num imgs x size of grid x 128 (Sift features) tensor
-        for images in os.listdir(training_images_dir):
-            DataProviderSURF.extract_features(training_images_dir + "/" + images)
+        termination_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+        bow_model = cv2.BOWKMeansTrainer(kwargs.pop("num_clusters", 500), termination_criteria)
+
+        for image_file in os.listdir(training_images_dir):
+            image_path = training_images_dir + "/" + image_file
+            print(f"Image {image_path}")
+            cv_image = DataProviderSURF.read_image(image_path, self._resize_image)
+            descriptors = DataProviderSURF.extract_features(cv_image, self._patch_size)
+            bow_model.add(descriptors[1])
 
         # use sklearn k-means clustering class to find x number of clusters
+        clusters = bow_model.cluster()
 
         # iterate over each image, and perform vector quantization
 
@@ -40,19 +46,50 @@ class DataProviderSURF(object):
         return
 
     @staticmethod
-    def extract_features(image_file, image_resize=(), grid_size=()):
-        image = cv.imread(image_file)
+    def read_image(image_file, resize_image=()):
+        """
+        Read an image and resize it, if necessary
 
-        if not image:
+        :param image_file: absolute image path
+        :param resize_image: (x, y) tuple for new image dimensions
+        :return: cv2 image
+        """
+
+        cv_image = cv2.imread(image_file)
+
+        if cv_image is None:
             raise RuntimeError(f"Unable to open {image_file}")
 
-        if len(image_resize) > 0:
-            image = cv.resize(image, image_resize)
+        if len(resize_image) > 0:
+            cv_image = cv2.resize(cv_image, resize_image)
 
-        # compute key points...just the center of the patch?
+        return cv_image
 
-        # compute descriptors for each key point
+    @staticmethod
+    def extract_features(image, patch_size=16):
+        """
+        Computes features based on the patch size
 
-        # return descriptors
+        :param image: input cv2 image
+        :param patch_size: size of the patches in the grid
+        :return: list of SURF descriptors
+        """
 
-        return
+        # compute key points, center of the patch; rows and cols are reversed
+        key_points = []
+        blob_size = int(math.floor(patch_size / 2))
+        start_loc = blob_size
+        for x_loc in range(start_loc, image.shape[1], patch_size):
+            for y_loc in range(start_loc, image.shape[0], patch_size):
+                key_points.append(cv2.KeyPoint(x_loc, y_loc, blob_size))
+
+        surf = nonfree.SURF_create(extended=True)
+        descriptors = surf.compute(image, key_points)
+
+        # note~ DEBUG CODE
+        # key_point_image = cv2.drawKeypoints(image, key_points, None)
+        # cv2.imshow("none", key_point_image)
+        # cv2.waitKey(0)
+        # kp, desc = surf.detectAndCompute(image, None)
+
+        return descriptors
--- anomaly_img_features/main.py
+++ anomaly_img_features/main.py
@@ -1,4 +1,5 @@
+import numpy as np
 import anomaly_img_features.data_provider.data_provider_sift as sift_provider
 
-
-sift_provider.DataProviderSURF("/Users/harpreetsingh/Downloads/airfield/neg_debug")
+sift_provider.DataProviderSURF("/Users/harpreetsingh/Downloads/airfield/neg", num_clusters=1000, resize_image=(800, 450))
+# sift_provider.DataProviderSURF.extract_features(np.zeros((56, 56)))
