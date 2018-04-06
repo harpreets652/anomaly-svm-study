10a210215cbdc33fd2a8e706329946d78374a352
harpreetsingh <harpreets@nevada.unr.edu>
2018-04-05 22:31:09 -0700

    saving model

--- anomaly_img_features/main.py
+++ anomaly_img_features/main.py
@@ -1,9 +1,27 @@
-import numpy as np
+import pickle
 import anomaly_img_features.data_provider.data_provider_sift as sift_provider
 
-sift_provider.DataProviderSURF("/Users/harpreetsingh/Downloads/airfield/neg_debug",
-                               num_clusters=3,
-                               resize_image=(400, 225),
-                               patch_size=32)
 
-# sift_provider.DataProviderSURF.extract_features(np.zeros((56, 56)))
+def save_model(output_file, model):
+    with open(output_file, 'w') as model_file:
+        pickle.dump(model, model_file)
+    return
+
+
+def load_model(model_file):
+    with open(model_file, 'r') as file:
+        model = pickle.load(file)
+    return model
+
+
+def main():
+    sift_provider.DataProviderSURF("/Users/harpreetsingh/Downloads/airfield/neg_debug",
+                                   num_clusters=3,
+                                   resize_image=(400, 225),
+                                   patch_size=32)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
