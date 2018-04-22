7c65fba0a8828a0dedb98867c9b78411683207a6
harpreetsingh <harpreets@nevada.unr.edu>
2018-04-22 16:19:06 -0700

    ResNet 50 deep network for feature extraction

--- anomaly_img_features/data_provider/data_provider_resnet50.py
+++ anomaly_img_features/data_provider/data_provider_resnet50.py
@@ -1,22 +1,20 @@
 import os
 from tensorflow.contrib.keras import applications
-import tensorflow.contrib.keras as keras
 import numpy as np
 
 import anomaly_img_features.data_provider.abstract_data_provider as abstract_provider
 
 
-class DataProviderVGG16(abstract_provider.AbstractDataProvider):
+class DataProviderResNet50(abstract_provider.AbstractDataProvider):
     def __init__(self, training_images_dir):
         """
-        This data provider utilizes VGG 16 deep network to extract features for each image
-        - feature vector size: 4096
+        This data provider utilizes ResNet 50 deep network to extract features for each image
+        - feature vector size: 2048
 
         :param training_images_dir: (string) directory containing training data
         """
 
-        base_model = applications.VGG16()
-        self._model = keras.models.Model(inputs=base_model.input, outputs=base_model.get_layer("fc2").output)
+        self._model = applications.resnet50.ResNet50(include_top=False, pooling="avg", input_shape=(224, 224, 3))
 
         training_x_list = []
         for image_file in os.listdir(training_images_dir):
@@ -24,31 +22,32 @@ class DataProviderVGG16(abstract_provider.AbstractDataProvider):
                 continue
 
             image_file_path = training_images_dir + "/" + image_file
-            cv_image = DataProviderVGG16.read_image(image_file_path, (224, 224))
+            cv_image = DataProviderResNet50.read_image(image_file_path, (224, 224))
             cv_image = np.expand_dims(cv_image, axis=0)
             cv_image = cv_image.astype("float32")
-            cv_image = applications.vgg16.preprocess_input(cv_image)
+            cv_image = applications.resnet50.preprocess_input(cv_image)
 
             features = self._model.predict(cv_image)
             training_x_list.append(features)
 
         self._X = np.vstack(training_x_list)
+
         return
+
+    def get_training_data(self):
+        return self._X
 
     def get_image_descriptor(self, image_path):
         """
-        Compute image descriptor from last fully connected layer from VGG 16
+        Compute image descriptor from last average pooling layer of ResNet50
 
         :param image_path: (string) path to the image
         :return: (ndarray) numpy array
         """
 
-        cv_image = DataProviderVGG16.read_image(image_path, (224, 224))
+        cv_image = DataProviderResNet50.read_image(image_path, (224, 224))
         cv_image = np.expand_dims(cv_image, axis=0)
         cv_image = cv_image.astype("float32")
-        cv_image = applications.vgg16.preprocess_input(cv_image)
+        cv_image = applications.resnet50.preprocess_input(cv_image)
 
         return self._model.predict(cv_image)
-
-    def get_training_data(self):
-        return self._X
--- anomaly_img_features/main.py
+++ anomaly_img_features/main.py
@@ -1,7 +1,10 @@
 import os
 import pickle
+import numpy as np
+
 import anomaly_img_features.data_provider.data_provider_surf as surf_provider
 import anomaly_img_features.data_provider.data_provider_vgg16 as vgg_provider
+import anomaly_img_features.data_provider.data_provider_resnet50 as resnet_provider
 import anomaly_img_features.anomaly_classifier as anomaly_classifier
 
 
@@ -19,14 +22,15 @@ def load_model(model_file):
 
 def main():
     training_data_dir = "/Users/harpreetsingh/Downloads/airfield/pos"
-    data_provider = vgg_provider.DataProviderVGG16(training_data_dir)
+    data_provider = resnet_provider.DataProviderResNet50(training_data_dir)
 
     classifier = anomaly_classifier.AnomalyClassifier(data_provider)
 
     test_data_dir = "/Users/harpreetsingh/Downloads/airfield/neg"
 
-    correct_prediction_counter = 0
     total_num_images = 0
+    pred_map = {-1: 0, 1: 1}
+    pred_count = np.zeros((2, 1))
     for test_image in os.listdir(test_data_dir):
         if not test_image.endswith(".jpg"):
             continue
@@ -34,13 +38,12 @@ def main():
         test_image_path = test_data_dir + "/" + test_image
         prediction = classifier.predict(test_image_path)
 
-        if prediction[0] == -1:
-            correct_prediction_counter += 1
+        pred_count[pred_map[prediction[0]]] += 1
         total_num_images += 1
 
         print(f"image: {test_image_path}, prediction: {prediction}")
 
-    print(f"Accuracy: {correct_prediction_counter/total_num_images}")
+    print(f"Results:\n{pred_count}\n total number of image: {total_num_images}")
 
     return
 
