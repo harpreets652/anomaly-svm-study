83cd33faa8645556daf4a2f1084a078ed9d1fcdb
harpreetsingh <harpreets@nevada.unr.edu>
2018-04-23 10:52:00 -0700

    inception v3 dataset

---
+++ anomaly_img_features/data_provider/data_provider_inceptv3.py
@@ -0,0 +1,54 @@
+import os
+from tensorflow.contrib.keras import applications
+import numpy as np
+
+import anomaly_img_features.data_provider.abstract_data_provider as abstract_provider
+
+
+class DataProviderInception(abstract_provider.AbstractDataProvider):
+    def __init__(self, training_images_dir):
+        """
+        This data provider utilizes inception V3 deep network to extract features for each image
+        - feature vector size: 2048
+
+        :param training_images_dir: (string) directory containing training data
+        """
+        self._model = applications.inception_v3.InceptionV3(include_top=False,
+                                                            pooling="avg",
+                                                            input_shape=(299, 299, 3))
+
+        training_x_list = []
+        for image_file in os.listdir(training_images_dir):
+            if not image_file.endswith(".jpg"):
+                continue
+
+            image_file_path = training_images_dir + "/" + image_file
+            cv_image = DataProviderInception.read_image(image_file_path, (299, 299))
+            cv_image = np.expand_dims(cv_image, axis=0)
+            cv_image = cv_image.astype("float32")
+            cv_image = applications.inception_v3.preprocess_input(cv_image)
+
+            features = self._model.predict(cv_image)
+            training_x_list.append(features)
+
+        self._X = np.vstack(training_x_list)
+
+        return
+
+    def get_training_data(self):
+        return self._X
+
+    def get_image_descriptor(self, image_path):
+        """
+        Compute image descriptor from last average pooling layer of InceptionV3
+
+        :param image_path: (string) path to the image
+        :return: (ndarray) numpy array
+        """
+
+        cv_image = DataProviderInception.read_image(image_path, (299, 299))
+        cv_image = np.expand_dims(cv_image, axis=0)
+        cv_image = cv_image.astype("float32")
+        cv_image = applications.resnet50.preprocess_input(cv_image)
+
+        return self._model.predict(cv_image)
--- anomaly_img_features/main.py
+++ anomaly_img_features/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import anomaly_img_features.data_provider.data_provider_surf as surf_provider
 import anomaly_img_features.data_provider.data_provider_vgg16 as vgg_provider
 import anomaly_img_features.data_provider.data_provider_resnet50 as resnet_provider
+import anomaly_img_features.data_provider.data_provider_inceptv3 as inception
 import anomaly_img_features.anomaly_classifier as anomaly_classifier
 
 
@@ -20,22 +21,17 @@ def load_model(model_file):
     return model
 
 
-def main():
-    training_data_dir = "/Users/harpreetsingh/Downloads/airfield/pos"
-    data_provider = resnet_provider.DataProviderResNet50(training_data_dir)
-
-    classifier = anomaly_classifier.AnomalyClassifier(data_provider)
-
-    test_data_dir = "/Users/harpreetsingh/Downloads/airfield/neg"
-
+def run_test_set(classifier, test_set_dir):
     total_num_images = 0
     pred_map = {-1: 0, 1: 1}
-    pred_count = np.zeros((2, 1))
-    for test_image in os.listdir(test_data_dir):
+    pred_count = np.zeros(2)
+
+    print(f"testing {test_set_dir}")
+    for test_image in os.listdir(test_set_dir):
         if not test_image.endswith(".jpg"):
             continue
 
-        test_image_path = test_data_dir + "/" + test_image
+        test_image_path = test_set_dir + "/" + test_image
         prediction = classifier.predict(test_image_path)
 
         pred_count[pred_map[prediction[0]]] += 1
@@ -44,6 +40,21 @@ def main():
         print(f"image: {test_image_path}, prediction: {prediction}")
 
     print(f"Results:\n{pred_count}\n total number of image: {total_num_images}")
+
+    return
+
+
+def main():
+    training_data_dir = "/Users/harpreetsingh/Downloads/airfield/pos_big"
+    data_provider = inception.DataProviderInception(training_data_dir)
+
+    classifier = anomaly_classifier.AnomalyClassifier(data_provider)
+
+    test_data_dir = "/Users/harpreetsingh/Downloads/airfield/neg"
+    run_test_set(classifier, test_data_dir)
+
+    test_data_dir = "/Users/harpreetsingh/Downloads/airfield/pos_val"
+    run_test_set(classifier, test_data_dir)
 
     return
 
