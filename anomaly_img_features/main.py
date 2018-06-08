74b27e746b0766c2d51cb02eefeae160df5f3d99
harpreetsingh <harpreets@nevada.unr.edu>
2018-06-08 10:10:48 -0700

    commented out pca

--- anomaly_img_features/main.py
+++ anomaly_img_features/main.py
@@ -52,34 +52,35 @@ def main():
     #                                                num_clusters=200,
     #                                                resize_image=(400, 225),
     #                                                patch_size=16)
-    data_provider = resnet_provider.DataProviderResNet50(training_data_dir)
+    data_provider = inception.DataProviderInception(training_data_dir)
+    print(f"Using {type(data_provider)}")
 
-    # note~ debug code...
+    # note~ debug code...==============================
     print("performing pca")
-    provider_pca_wrapper = pca_wrapper.DataProviderPCAWrapper(provider=data_provider,
-                                                              num_principle_components=2)
+    # provider_pca_wrapper = pca_wrapper.DataProviderPCAWrapper(provider=data_provider,
+    #                                                           num_principle_components=10)
 
-    train_data_plt = provider_pca_wrapper.get_training_data()
-    plt.scatter(train_data_plt[:, 0], train_data_plt[:, 1], c='blueviolet', s=40, edgecolors='k')
+    # train_data_plt = provider_pca_wrapper.get_training_data()
+    # plt.scatter(train_data_plt[:, 0], train_data_plt[:, 1], c='blueviolet', s=40, edgecolors='k')
+    #
+    # neg_list = []
+    # test_data_dir = "/Users/harpreetsingh/Downloads/airfield/neg"
+    # for test_image in os.listdir(test_data_dir):
+    #     if not test_image.endswith(".jpg"):
+    #         continue
+    #
+    #     test_image_path = test_data_dir + "/" + test_image
+    #     x_i = provider_pca_wrapper.get_image_descriptor(test_image_path)
+    #     neg_list.append(x_i)
+    #
+    # test_x = np.vstack(neg_list)
+    # plt.scatter(test_x[:, 0], test_x[:, 1], c='gold', s=40, edgecolors='k')
+    # plt.show()
 
-    neg_list = []
-    test_data_dir = "/Users/harpreetsingh/Downloads/airfield/neg"
-    for test_image in os.listdir(test_data_dir):
-        if not test_image.endswith(".jpg"):
-            continue
-
-        test_image_path = test_data_dir + "/" + test_image
-        x_i = provider_pca_wrapper.get_image_descriptor(test_image_path)
-        neg_list.append(x_i)
-
-    test_x = np.vstack(neg_list)
-    plt.scatter(test_x[:, 0], test_x[:, 1], c='gold', s=40, edgecolors='k')
-    plt.show()
-
-    print("pca done")
+    # print("pca done")
     # Note~ t===============================
 
-    classifier = anomaly_classifier.AnomalyClassifier(provider_pca_wrapper, nu=0.1, gamma=0.1)
+    classifier = anomaly_classifier.AnomalyClassifier(data_provider, nu=0.1, gamma=0.1)
 
     support_vectors = classifier.get_support_vectors()
     print("number of support vectors: \n", support_vectors.shape)
