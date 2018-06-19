54ca6029de9868a5cbfa3333dfbccca897718f5a
harpreetsingh <harpreets@nevada.unr.edu>
2018-06-19 13:49:09 -0700

    adding conditional check in custom loss...need to change it to only use keras backend functions

--- deep_one_class_features/custom_loss.py
+++ deep_one_class_features/custom_loss.py
@@ -1,13 +1,17 @@
 from keras import backend as k
+import tensorflow as tf
 
-discriminative_loss = 0.0
-scalar_lambda = 0.1
+discriminative_loss = 1.0
+lambda_coefficient = 0.1
 
 
 def compute_total_loss(y_true, y_pred):
-    mean = k.mean(y_pred, axis=0)
+    if type(y_pred.shape[0]) is not type(int):
+        return tf.constant(0.0)
 
+    mean = k.mean(y_pred, axis=0)
     compactness_loss = 0.0
+
     for i in range(y_pred.shape[0]):
         x_i = y_pred[i]
         mean_minus_i = (y_pred.shape[0] * mean - x_i) / (y_pred.shape[0] - 1)
@@ -16,6 +20,6 @@ def compute_total_loss(y_true, y_pred):
 
     compactness_loss *= 1 / (y_pred.shape[0] * y_pred.shape[1])
 
-    total_loss = discriminative_loss + (scalar_lambda * compactness_loss)
+    total_loss = discriminative_loss + (lambda_coefficient * compactness_loss)
 
-    return total_loss
+    return tf.constant(total_loss)
