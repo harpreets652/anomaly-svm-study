500dc41b0290f37acc6e331128b6b86ff6bd6bba
harpreetsingh <harpreets@nevada.unr.edu>
2018-06-18 22:20:03 -0700

    initial implementation of DOC and custom loss...need to test

--- deep_one_class_features/custom_loss.py
+++ deep_one_class_features/custom_loss.py
@@ -1,8 +1,21 @@
-import keras
+from keras import backend as k
 
-loss_discriminative = 1000.0
+discriminative_loss = 0.0
+scalar_lambda = 0.1
 
 
-# todo: this is able to access the loss_discriminative value
 def compute_total_loss(y_true, y_pred):
-    return loss_discriminative * keras.losses.mean_squared_error(y_true, y_pred)
+    mean = k.mean(y_pred, axis=0)
+
+    compactness_loss = 0.0
+    for i in range(y_pred.shape[0]):
+        x_i = y_pred[i]
+        mean_minus_i = (y_pred.shape[0] * mean - x_i) / (y_pred.shape[0] - 1)
+        z_i = x_i - mean_minus_i
+        compactness_loss += k.dot(z_i.T, z_i)
+
+    compactness_loss *= 1 / (y_pred.shape[0] * y_pred.shape[1])
+
+    total_loss = discriminative_loss + (scalar_lambda * compactness_loss)
+
+    return total_loss
